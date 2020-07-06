98e2eee72760ee6e56a00590a6aa350ad54d637f
Sarah Wist <sarahwist107@gmail.com>
2020-07-06 14:43:23 -0700

    added } to line 15

--- config/connection.js
+++ config/connection.js
@@ -6,12 +6,13 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db",
+  });
+}
 
 // Make connection
 connection.connect(function (err) {
